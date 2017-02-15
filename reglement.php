f57c49ec468bd827b296e706845a239460b7488f
stan.daniels_roth <stan.daniels-roth@epitech.eu>
2017-02-15 23:56:30 +0100

    structuring reglement.php

--- reglement.php
+++ reglement.php
@@ -20,7 +20,7 @@
 
                          <span class="TAB">Le</span> projet RobinsonMC est un projet venu tout droit de Mrstandu33,
                          diffuseur de contenu en live sur twitch.tv, voulant alors
-                         rendre hommage à l'ancien serveur Hystoria qui, dans le
+                         rendre hommage à l'ancien serveur RéalityCraft qui, dans le
                          passé fut baptisé "Robinson". En aucun cas le projet RobinsonMC
                          n'est affilié de près ou de loin à Hystoria ou à son créateur.<br><br>
 
@@ -30,11 +30,47 @@
                          également un serveur vocal TeamSpeak <span class="LINK">(ts.robinsonmc.fr)</span>
                          d'une série de serveur Multi-jeux et d'un site web <span class="LINK">(www.robinsonmc.fr)</span>.<br><br>
 
+                     <h4>B/ Le serveur Minecraft RobinsonMC</h4>
+
+                     <h4>C/ Les infrastructures</h4>
+
+                          <h5>a/ TeamSpeak</h5>
+                          <h5>b/ Site Web</h5>
+                          <h5>c/ Forum</h5>
+                          <h5>d/ Chaîne Communautaire</h5>
+<br><br><br>
+                    <h3>II/ Droits du Staff</h3>
+
+                          <h4>A/ Sanctions</h4>
+                          <h4>B/ Capacités</h4>
+<br><br><br>
+                    <h3>III/ Devoirs du Staff</h3>
+
+                          <h4>A/ Sanctions</h4>
+                          <h4>B/ Décisions</h4>
+<br><br><br>
+                    <h3>IV/ Droits du Joueur</h3>
+
+                          <h4>A/ Construction</h4>
+                          <h4>B/ Relation</h4>
+                          <h4>C/ Demande d'assistance</h4>
+<br><br><br>
+                     <h3>V/ Devoirs du Joueur</h3>
+
+                          <h4>A/ Respect</h4>
+                          <h4>B/ Sanctions</h4>
+<br><br><br>
+                     <h3>VI/ Sanctions</h3>
+
+                          <h4>A/ Présentation</h4>
+                          <h4>B/ Rapport Sanction/Gravité</h4>
+<br><br><br>
+
                          <span class="TAB">Le</span> Projet RobinsonMC n'est nullement apparenté à Mojang,
                          Microsoft, TeamSpeak, OVH ou VPSz.<br><br>
-
                      <span class="UPDATE">Page mise à jour le 15/02/2017 à 22:05.</span>
                      <br>
+
             </div>
 
        </div>
